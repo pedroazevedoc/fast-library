e3c533c007ae9abf1410779fc6c8cd8e48f7e50f
Pedro Azevedo <pedroazvdo.8@gmail.com>
2026-08-07 23:46:56 -0300

    refactor: update about page adding informations

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,11 +1,23 @@
+import { Link } from 'react-router-dom';
+import './about.css';
+
 function About() {
   return (
-    <div>
-      <h2>Sobre a Fast Library</h2>
-      <br />
+    <div className="about-container">
+      <h1>Sobre a Fast Library</h1>
       <p>
         A Fast Library é uma biblioteca de livros que oferece uma ampla variedade de títulos para todos os gostos.
       </p>
+      <p>
+        Nosso objetivo é fornecer aos leitores uma experiência agradável e acessível, permitindo que eles encontrem facilmente os livros que desejam.
+      </p>
+      <p>
+        Explore nossa coleção e descubra novos autores, gêneros e histórias emocionantes!
+      </p>
+
+      <span>Os dados dos livros são coletados da API da Open Library.</span>
+
+      <Link to="/">Voltar para a página inicial</Link>
     </div>
   );
 }
