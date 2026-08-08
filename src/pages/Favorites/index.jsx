11484671c23e39e1497b16f3b40ae3967084f092
Pedro Azevedo <pedroazvdo.8@gmail.com>
2026-08-07 22:53:28 -0300

    feat: add the book's author in each item of favorites page

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom";
 
 function Favorites() {
   const [loading, setLoading] = useState(true);
-  const [films, setFilms] = useState([]);
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
     const storedFavorites = localStorage.getItem('@favorites');
-    setFilms(JSON.parse(storedFavorites) || []);
+    setBooks(JSON.parse(storedFavorites) || []);
     setLoading(false);
   }, []);
 
   const handleDeleteFavorite = (key) => {
     return () => {
-      const updatedFavorites = films.filter((film) => film.key !== key);
-      setFilms(updatedFavorites);
+      const updatedFavorites = books.filter((book) => book.key !== key);
+      setBooks(updatedFavorites);
       localStorage.setItem('@favorites', JSON.stringify(updatedFavorites));
     };
   };
@@ -28,11 +28,11 @@ function Favorites() {
     );
   }
 
-  if (films.length === 0) {
+  if (books.length === 0) {
     return (
       <div className="no-favorites">
-        <p>Nenhum filme favorito encontrado.</p>
-        <span>Adicione filmes aos favoritos para vê-los aqui.</span>
+        <p>Nenhum livro favorito encontrado.</p>
+        <span>Adicione livros aos favoritos para vê-los aqui.</span>
       </div>
     );
   }
@@ -42,13 +42,16 @@ function Favorites() {
       <div className="favorites-list">
         <h1>Favoritos</h1>
         <ul>
-          {films.map((film) => {
+          {books.map((book) => {
             return (
-              <li key={film.key} className="favorite-item">
-                <span>{film.title}</span>
-                <div>
-                  <Link to={`/book/${film.key.replace('/works/', '')}`}>Ver Detalhes</Link>
-                  <button onClick={handleDeleteFavorite(film.key)}>Excluir</button>
+              <li key={book.key} className="favorite-item">
+                <div className="favorite-info">
+                  <p>{book.title}</p>
+                  <span>{book.authors ? book.authors?.[0]?.author?.name : 'Autor desconhecido'}</span>
+                </div>
+                <div className="favorite-actions">
+                  <Link to={`/book/${book.key.replace('/works/', '')}`}>Ver Detalhes</Link>
+                  <button onClick={handleDeleteFavorite(book.key)}>Excluir</button>
                 </div>
               </li>
             )
